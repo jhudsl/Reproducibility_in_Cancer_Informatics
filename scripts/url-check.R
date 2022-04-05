14b59786dfa1d754edb9bedecfb86e43648f5c40
jhudsl-robot <null>
2022-04-05 12:56:18 +0000

    🔄 Synced local 'scripts/url-check.R' with remote 'scripts/url-check.R'
    release-null

--- scripts/url-check.R
+++ scripts/url-check.R
@@ -9,6 +9,15 @@ library(magrittr)
 # Find .git root directory
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 
+ignore_urls_file <- file.path(root_dir, "resources", "ignore-urls.txt")
+
+if (!file.exists(ignore_urls_file)) {
+  message(paste("No ignore URLs text file found at:", ignore_urls_file, "downloading one from the main OTTR Template repo"))
+  download.file("https://raw.githubusercontent.com/jhudsl/OTTR_Template/main/resources/ignore-urls.txt",
+               destfile = ignore_urls_file)
+}
+ignore_urls <- readLines(ignore_urls_file)
+
 output_file <- file.path(root_dir, 'check_reports', 'url_checks.tsv')
 
 if (!dir.exists('check_reports')) {
@@ -28,11 +37,12 @@ test_url <- function(url) {
 get_urls <- function(file) {
   # Read in a file and return the urls from it
   content <- readLines(file)
-  content <- grep("http|com$|www", content, value = TRUE)
-  url_pattern <- "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+  content <- grep("http[s]?://", content, value = TRUE)
+  url_pattern <- "http[s]?://.+?[\"|\\)| |,|}]"
   urls <- stringr::str_extract(content, url_pattern)
+  urls <- urls[!is.na(urls)]
   if (length(urls) > 0 ){
-    urls <- gsub(")$|)\\.$", "", urls)
+    urls <- gsub("\\)$|\"|)$", "", urls)
     urls_status <- sapply(urls, test_url)
     url_df <- data.frame(urls, urls_status, file)
     return(url_df)
@@ -43,22 +53,23 @@ get_urls <- function(file) {
 all_urls <- lapply(files, get_urls)
 
 # Write the file
-all_urls_df <- dplyr::bind_rows(all_urls)
+all_urls_df <- dplyr::bind_rows(all_urls) %>%
+  dplyr::filter(!is.na(urls))
 
 if (nrow(all_urls_df) > 0) {
-  all_urls_df <- all_urls_df %>%
+  failed_urls_df <- all_urls_df %>%
     dplyr::filter(urls_status == "failed")
 } else {
-  all_urls_df <- data.frame(errors = NA)
+  failed_urls_df <- data.frame(errors = NA)
 }
 
-# Print out how many spell check errors
-write(nrow(all_urls_df), stdout())
+failed_urls_df <- failed_urls_df %>%
+  dplyr::filter(!(urls %in% ignore_urls))
 
 # Save spell errors to file temporarily
-readr::write_tsv(all_urls_df, output_file)
+readr::write_tsv(failed_urls_df, output_file)
 
 message(paste0("Saved to: ", output_file))
 
 # Print out how many spell check errors
-write(nrow(all_urls_df), stdout())
+write(nrow(failed_urls_df), stdout())
